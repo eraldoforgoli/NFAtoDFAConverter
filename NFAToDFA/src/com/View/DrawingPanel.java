df826e09015e4cfa2a8fc734bcd52209e37ea607
eraldo <eraldoforgoli@gmail.com>
2019-02-18 20:32:54 +0100

    Made changes in the way automata is draws. View package

--- NFAToDFA/src/com/View/DrawingPanel.java
+++ NFAToDFA/src/com/View/DrawingPanel.java
@@ -2,8 +2,7 @@ package com.View;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+
 import java.util.List;
 
 public class DrawingPanel extends JPanel {
@@ -13,34 +12,15 @@ public class DrawingPanel extends JPanel {
 		dataCalculator = new DataCalculator(file);
 		setLayout(null);
 		setBackground(Color.WHITE);
-		addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				System.out.println("clicked at - " + e.getPoint());
-			}
-		});
-		JLabel l = new JLabel("starting state - " + dataCalculator.getStart());
-		l.setBounds(10, 10, 100, 20);
-		add(l);
+
+		JLabel startingStateLabel = new JLabel("starting state - " + dataCalculator.getStart());
+		startingStateLabel.setFont(new Font("Dialog", 3, 13));
+
+		startingStateLabel.setBounds(10, 10, 100, 20);
+		add(startingStateLabel);
 	}
 
 	private Color getColor(int i) {
-		switch (i % 5) {
-		case 0:
-			return Color.BLUE;
-
-		case 1:
-			return Color.GREEN;
-
-		case 2:
-			return Color.MAGENTA;
-
-		case 3:
-			return Color.GRAY;
-
-		case 4:
-			return Color.ORANGE;
-		}
 		return Color.BLACK;
 	}
 
@@ -48,29 +28,37 @@ public class DrawingPanel extends JPanel {
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
+		g2.setStroke(new BasicStroke(2));
+
 		Circle[] states = dataCalculator.getCirclesData();
 
-		g.setColor(Color.BLACK);
+		g2.setColor(Color.BLACK);
 		for (Circle c : states) {
 			drawCircle(g, c);
 		}
 
 		SelfLoop sls[] = dataCalculator.getSelfLoops();
 		for (SelfLoop s : sls) {
-			drawSelfLoops(g, s);
+			drawSelfLoops(g2, s);
 		}
 
 		List<Arrow> trans = dataCalculator.getArrowsData();
 		for (Arrow a : trans) {
-			drawArrow(g, a);
+			drawArrow(g2, a);
 		}
 
 		for (Arrow a : trans) {
-			drawString(g, a);
+			drawString(g2, a);
 		}
 	}
 
 	private void drawString(Graphics g, Arrow a) {
+		Graphics2D g2 = (Graphics2D) g;
+		this.makeLineThicker(g2);
+		this.MakeFontItalic(g2);
+
 		int mul;
 		if (a.input_symbol.length() % 2 == 0) {
 			mul = a.input_symbol.length() / 2;
@@ -78,20 +66,25 @@ public class DrawingPanel extends JPanel {
 			mul = a.input_symbol.length() / 2 + 1;
 		}
 
-		g.setColor(Color.WHITE);
-		g.fillRect(a.string_pos.x, a.string_pos.y - 10, 10 * mul, 10);
+		g2.setColor(Color.WHITE);
+		g2.fillRect(a.string_pos.x, a.string_pos.y - 10, 10 * mul, 10);
 
-		g.setColor(getColor(a.id));
-		g.drawString(a.input_symbol, a.string_pos.x, a.string_pos.y);
+		g2.setColor(getColor(a.id));
+		g2.drawString(a.input_symbol, a.string_pos.x, a.string_pos.y);
 	}
 
 	private void drawSelfLoops(Graphics g, SelfLoop s) {
-		System.out.println(s);
-		g.setColor(getColor(s.id));
 
-		g.drawArc(s.centre.x - s.radius, s.centre.y - s.radius, 2 * s.radius, 2 * s.radius, 112 - s.theta, -224);
-		g.drawLine(s.p1.x, s.p1.y, s.end.x, s.end.y);
-		g.drawLine(s.p2.x, s.p2.y, s.end.x, s.end.y);
+		Graphics2D g2 = (Graphics2D) g;
+		this.makeLineThicker(g2);
+		this.MakeFontItalic(g2);
+
+		System.out.println(s);
+		g2.setColor(getColor(s.id));
+
+		g2.drawArc(s.centre.x - s.radius, s.centre.y - s.radius, 2 * s.radius, 2 * s.radius, 112 - s.theta, -224);
+		g2.drawLine(s.p1.x, s.p1.y, s.end.x, s.end.y);
+		g2.drawLine(s.p2.x, s.p2.y, s.end.x, s.end.y);
 
 		int mul;
 		if (s.input_symbol.length() % 2 == 0) {
@@ -100,33 +93,54 @@ public class DrawingPanel extends JPanel {
 			mul = s.input_symbol.length() / 2 + 1;
 		}
 
-		g.setColor(Color.WHITE);
-		g.fillRect(s.string_pos.x, s.string_pos.y - 10, 10 * mul, 10);
+		g2.setColor(Color.WHITE);
+		g2.fillRect(s.string_pos.x, s.string_pos.y - 10, 10 * mul, 10);
 
-		g.setColor(getColor(s.id));
-		g.drawString(s.input_symbol, s.string_pos.x, s.string_pos.y);
+		g2.setColor(getColor(s.id));
+		g2.drawString(s.input_symbol, s.string_pos.x, s.string_pos.y);
 	}
 
 	private void drawArrow(Graphics g, Arrow a) {
+
+		Graphics2D g2 = (Graphics2D) g;
+		this.makeLineThicker(g2);
+		this.MakeFontItalic(g2);
+
 		System.out.println(a);
 		Color c = getColor(a.id);
-		g.setColor(c);
+		g2.setColor(c);
 		for (int i = 1; i < a.pts.size(); i++) {
 			g.drawLine(a.getPointAt(i - 1).x, a.getPointAt(i - 1).y, a.getPointAt(i).x, a.getPointAt(i).y);
 		}
-		g.drawLine(a.p1.x, a.p1.y, a.p3.x, a.p3.y);
-		g.drawLine(a.p2.x, a.p2.y, a.p3.x, a.p3.y);
+		g2.drawLine(a.p1.x, a.p1.y, a.p3.x, a.p3.y);
+		g2.drawLine(a.p2.x, a.p2.y, a.p3.x, a.p3.y);
 	}
 
 	private void drawCircle(Graphics g, Circle c) {
+
+		Graphics2D g2 = (Graphics2D) g;
+		this.makeLineThicker(g2);
+		this.MakeFontItalic(g2);
 		System.out.println(c);
 
 		if (c.isFinal()) {
-			g.drawArc(c.centre.x - Circle.RADIUS + 5, c.centre.y - Circle.RADIUS + 5, Circle.DIAMETER - 10,
+			g2.drawArc(c.centre.x - Circle.RADIUS + 5, c.centre.y - Circle.RADIUS + 5, Circle.DIAMETER - 10,
 					Circle.DIAMETER - 10, 0, 360);
 		}
 
-		g.drawArc(c.centre.x - Circle.RADIUS, c.centre.y - Circle.RADIUS, Circle.DIAMETER, Circle.DIAMETER, 0, 360);
-		g.drawString(c.id + "", c.centre.x, c.centre.y);
+		g2.drawArc(c.centre.x - Circle.RADIUS, c.centre.y - Circle.RADIUS, Circle.DIAMETER, Circle.DIAMETER, 0, 360);
+		g2.drawString(c.id + "", c.centre.x - 5, c.centre.y + 5);
+	}
+
+	public void makeLineThicker(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
+		g2.setStroke(new BasicStroke(2));
+	}
+
+	public void MakeFontItalic(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		Font myFont = new Font("Dialog", 3, 17);
+		g2.setFont(myFont);
 	}
 }
